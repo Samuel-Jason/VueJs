da924e6caccf56a1ff9a1365a05212a0add4d027
Samuel <samuelljason@gmail.com>
2025-01-25 23:31:23 -0300

    Aplicando eventos com EMIT, onde acontece evento no componente filho e ele notifica o componente pai

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,15 +1,27 @@
 <template>
-    <img v-show="foto" :src="avatar" :alt="descricao">
+    <div>
+        <img :src="avatar" :alt="descricao">
+        <MudarImagem @mudar-imagem="TrocarImagem"/>
+    </div>
 </template>
-
 <script>
+import MudarImagem from './MudarImagem.vue';
+
 export default {
     name: 'Picture',
+    components: {
+        MudarImagem
+    },
     data() {
         return {
             avatar: '/img/avatar.png',
             descricao: 'Samuel Jason',
             foto: false
+        }
+    },
+    methods: {
+        TrocarImagem(newImage) {
+            this.avatar = newImage;
         }
     }
 }
